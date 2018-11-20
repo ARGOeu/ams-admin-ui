68f2f859a37a1591562e345357d45f658eba8502
Konstantinos Kagkelidis <kaggis@gmail.com>
2018-11-20 16:39:46 +0200

    ARGO-1474 Introduce simple routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import UserManager from './UserManager';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import About from './About'
 
 // Main React App entrypoint. Uses the main top-level element UserManager which
 // is responsible for retrieving and displaying user data
 class App extends Component {
   render() {
-    return (
+
+    // Create a navigation bar with links to different routes
+    // Create a BrowserRouter Wrapper above App
+    // Use Switch with two Routers to render two views: UserManager and About
+    return (<BrowserRouter>
+     
       <div className="App">
-       <UserManager/>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+        <main>
+        <Switch>
+          <Route  exact path='/' component={UserManager} />
+          <Route  exact path='/about' component={About} />
+        </Switch>
+        </main>
       </div>
+      
+    </BrowserRouter>
     );
   }
 }
