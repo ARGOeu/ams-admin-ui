db8a0ea804773ec525b11718100e80da9cd9c3db
Konstantinos Kagkelidis <kaggis@gmail.com>
2019-07-05 11:09:04 +0300

    ARGO-1863 User views based on roles ARGO-1861 Project views based on role

--- src/config.js
+++ src/config.js
@@ -2,10 +2,17 @@
 // Replace endpoint value with target AMS endpoint you want to retrieve data from
 var config = {
 	endpoint: "localhost",
+<<<<<<< HEAD
 	available_endpoints: ["localhost:8080", "remote-api.example.com"],
 	endpoint_colors: {
 		"localhost:8080":"#6E8DC4",
 		"remote-api.example.com":"#c46e7a"},
+=======
+	available_endpoints: ["localhost","remote-api.example.com"],
+	endpoint_colors: {
+		"remote-api.example.com":"red",
+		"localhost":"green"},
+>>>>>>> ARGO-1863 User views based on roles
 	service: "ams",
 	project_colors: {
 		"ARGO": "#769B9E",
