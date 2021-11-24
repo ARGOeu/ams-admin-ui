a39e516a454d9b478c89f74b37a4fc0be9edd103
Kostas Evangelou <kevangel@admin.grnet.gr>
2021-11-24 16:21:11 +0200

    Add msg-devel url enpoint to config file

--- src/config.js
+++ src/config.js
@@ -2,10 +2,11 @@
 // Replace endpoint value with target AMS endpoint you want to retrieve data from
 var config = {
 	endpoint: "localhost",
-	available_endpoints: ["localhost:8080", "remote-api.example.com"],
+	available_endpoints: ["localhost:8080", "remote-api.example.com","msg-devel.argo.grnet.gr"],
 	endpoint_colors: {
 		"localhost:8080":"#6E8DC4",
-		"remote-api.example.com":"#c46e7a"},
+		"remote-api.example.com":"#c46e7a",
+		"msg-devel.argo.grnet.gr":"#123abc"},
 	service: "ams",
 	project_colors: {
 		"ARGO": "#769B9E",
